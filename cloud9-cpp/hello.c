fa11445c6d90077a8190c58dbb6a522f5ced9412
Mike Lambert <lambert8@illinois.edu>
2016-08-11 22:34:12 +0000

    Restructured a bit so this scales slightly better

--- cloud9-cpp/hello.c
+++
@@ -1,8 +0,0 @@
-// C "Hello World" example
-#include <stdio.h>
- 
-int main()
-{
-  printf("Hello, World! (C)\n");
-  return 0;
-}
--- cloud9-cpp/hello.cpp
+++
@@ -1,9 +0,0 @@
-// C++ "Hello World" example
-#include <iostream>
-using namespace std;
- 
-int main()
-{
-  std::cout << "Hello, World! (C++)" << std::endl;
-  return 0;
-}
