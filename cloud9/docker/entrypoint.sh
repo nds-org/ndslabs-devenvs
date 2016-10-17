f64872f46a8ec6de44444a4d0ce2c023f1d2f6bf
Craig Willis <willis8@illinois.edu>
2016-10-17 16:52:43 +0000

    Added BIP, MTU and reverted storage driver to vfs

--- cloud9/docker/entrypoint.sh
+++ cloud9/docker/entrypoint.sh
@@ -6,8 +6,10 @@ if [ "$1" = "docker" ]; then
    $(which dind) docker daemon \
         --host=unix:///var/run/docker.sock \
         --host=tcp://0.0.0.0:2375 \
+        --bip=10.1.0.1/16 \
+        --mtu=1454 \
         --registry-mirror=http://docker-cache.default:5001 \
-        --storage-driver=overlay  > dind.log 2>&1 &
+        --storage-driver=vfs  > dind.log 2>&1 &
 
     nodejs /cloud9/server.js -p 80 -w /workspace
 else
