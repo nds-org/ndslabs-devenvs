336070128206a769bf82a0298663db634d391c00
Craig Willis <willis8@illinois.edu>
2016-10-19 02:29:41 +0000

    Reverted to vfs

--- cloud9/docker/entrypoint.sh
+++ cloud9/docker/entrypoint.sh
@@ -9,7 +9,7 @@ if [ "$1" = "docker" ]; then
         --bip=10.1.0.1/16 \
         --mtu=1404 \
         --registry-mirror=http://docker-cache.default:5001 \
-        --storage-driver=overlay  > dind.log 2>&1 &
+        --storage-driver=vfs  > dind.log 2>&1 &
 
 
     nodejs /cloud9/server.js -p 80 -w /workspace
